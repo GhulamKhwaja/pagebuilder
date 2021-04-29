6e75f2424dd363f282e21905cddc71dc92763bc0
ArunJH04 <51953327+ArunJH04@users.noreply.github.com>
2021-04-29 10:44:14 +0530

    device type drop down issue fixed

--- src/app/planning/list-items/list-items.component.ts
+++ src/app/planning/list-items/list-items.component.ts
@@ -1,71 +1,105 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Params, Router } from "@angular/router";
-import { PlanningService } from "../../planning/planning.service";
-import * as $ from "jquery";
-import { ServiceService } from "../../service.service";
-import { Subscription } from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { PlanningService } from '../../planning/planning.service';
+import * as $ from 'jquery';
+import { ServiceService } from '../../service.service';
+import { Subscription } from 'rxjs';
 
 @Component({
- selector: "app-list-items",
- templateUrl: "./list-items.component.html",
- styleUrls: ["./list-items.component.css"],
+  selector: 'app-list-items',
+  templateUrl: './list-items.component.html',
+  styleUrls: ['./list-items.component.css'],
 })
 export class ListItemsComponent implements OnInit {
- rightDivShow: boolean = false;
- leftDivShow: boolean = true;
- messageReceived: any;
- private subscriptionName: Subscription;
+  rightDivShow: boolean = false;
+  leftDivShow: boolean = true;
+  messageReceived: any;
+  private subscriptionName: Subscription;
 
- constructor(private router: Router, private route: ActivatedRoute, private planningService: PlanningService, private appService: ServiceService) {
-  this.subscriptionName = this.appService.getUpdate().subscribe((message) => {
-   //message contains the data sent from service
-   this.messageReceived = message.text;
-   this.pageName = this.messageReceived.pageName;
-   this.pageId = this.messageReceived.pageID;
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private planningService: PlanningService,
+    private appService: ServiceService
+  ) {
+    this.subscriptionName = this.appService.getUpdate().subscribe((message) => {
+      //message contains the data sent from service
+      this.messageReceived = message.text;
+      this.pageName = this.messageReceived.pageName;
+      this.pageId = this.messageReceived.pageID;
 
-   console.log(this.pageName, this.pageId);
-   this.planningService.getDeviceList(this.pageName).subscribe((data: any) => {
-    this.allDevices = data.Details;
-   });
-  });
- }
+      console.log(this.pageName, this.pageId);
+      this.planningService
+        .getDeviceList(this.pageName)
+        .subscribe((data: any) => {
+          this.allDevices = data.Details;
+        });
+    });
+  }
 
- allDevices: any = [];
- pageId;
- pageName;
- searchText;
+  allDevices: any = [];
+  pageId;
+  pageName;
+  searchText;
+  SelectedIDs: any = [];
 
- ngOnInit() {}
+  ngOnInit() {}
 
- routeToAddDetails() {
-  let url = "/planning/add-items/" + this.pageName + "/" + this.pageId + "";
-  console.log(url);
-  return url;
-  // this.router.navigate([
-  //   "/planning/add-items",
-  //   { pageName: this.pageName, id: this.pageId },
-  // ]);
- }
+  routeToAddDetails() {
+    let url = '/planning/add-items/' + this.pageName + '/' + this.pageId + '';
+    console.log(url);
+    return url;
+    // this.router.navigate([
+    //   "/planning/add-items",
+    //   { pageName: this.pageName, id: this.pageId },
+    // ]);
+  }
 
- leftDivClick() {
-  this.rightDivShow = true;
-  this.leftDivShow = false;
- }
+  selectID(event: any) {
+    let valueExists;
+    let id = this.SelectedIDs.indexOf(event.target.id);
+    if (this.SelectedIDs.indexOf(event.target.id) !== -1) {
+      valueExists = true;
+    } else {
+      valueExists = false;
+    }
+    if (event.target.checked && valueExists === false) {
+      this.SelectedIDs.push(event.target.id);
+    } else {
+      this.SelectedIDs.splice(id, 1);
+    }
 
- filterValues(event) {
-  this.searchText = event.target.value;
- }
- getRecord(elements) {
-  console.log(elements.id);
-  console.log(elements.type);
-  let elementDetails = { pageType: elements.type, elementID: elements.id };
-  this.sendElementMessage(elementDetails);
-  localStorage.setItem("pageType", elementDetails.pageType);
-  localStorage.setItem("elementID", elementDetails.elementID);
- }
+    localStorage.setItem('selectedIds', JSON.stringify(this.SelectedIDs));
+    // console.log(this.SelectedIDs);
+  }
 
- sendElementMessage(elementDetails): void {
-  // send message to subscribers via observable subject
-  this.appService.sendUpdate(elementDetails);
- }
+  deleteSelected() {
+    console.log('called delete');
+
+    this.SelectedIDs.forEach(function (obj) {
+      this.allDevices = this.allDevices.filter((item) => item.id !== obj.id);
+    });
+  }
+
+  leftDivClick() {
+    this.rightDivShow = true;
+    this.leftDivShow = false;
+  }
+
+  filterValues(event) {
+    this.searchText = event.target.value;
+  }
+  getRecord(elements) {
+    console.log(elements.id);
+    console.log(elements.type);
+    let elementDetails = { pageType: elements.type, elementID: elements.id };
+    this.sendElementMessage(elementDetails);
+    localStorage.setItem('pageType', elementDetails.pageType);
+    localStorage.setItem('elementID', elementDetails.elementID);
+  }
+
+  sendElementMessage(elementDetails): void {
+    // send message to subscribers via observable subject
+    this.appService.sendUpdate(elementDetails);
+  }
 }
--- src/app/planning/planning.service.ts
+++ src/app/planning/planning.service.ts
@@ -1,95 +1,159 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { environment } from "../../environments/environment";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 let headers = new HttpHeaders();
-headers = headers.set("Content-Type", "application/json; charset=utf-8");
+headers = headers.set('Content-Type', 'application/json; charset=utf-8');
 
 let menuHeaders = new HttpHeaders();
-menuHeaders = headers.set("Content-Type", "application/json; charset=utf-8").set("user_role", "1,2,3");
+menuHeaders = headers
+  .set('Content-Type', 'application/json; charset=utf-8')
+  .set('user_role', '1,2,3');
 
 @Injectable({
- providedIn: "root",
+  providedIn: 'root',
 })
 export class PlanningService {
- constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
- getDeviceList(pageName): Observable<any> {
-  if (pageName === "Device") {
-   return this.http.get(environment.baseUrl + environment.apiEndPoint.getAllDevice, { headers: headers });
+  getDeviceList(pageName): Observable<any> {
+    if (pageName === 'Device') {
+      return this.http.get(
+        environment.baseUrl + environment.apiEndPoint.getAllDevice,
+        { headers: headers }
+      );
+    }
+
+    if (pageName === 'Shelf') {
+      return this.http.get(
+        environment.webApiBaseUrl + environment.apiEndPoint.GetShelf,
+        { headers: headers }
+      );
+    }
+
+    if (pageName === 'Port') {
+      return this.http.get(
+        environment.webApiBaseUrl + environment.apiEndPoint.GetPorts,
+        { headers: headers }
+      );
+    }
+
+    if (pageName === 'Card') {
+      return this.http.get(
+        environment.webApiBaseUrl + environment.apiEndPoint.GetCard,
+        { headers: headers }
+      );
+    }
+
+    if (pageName === 'Link') {
+      return this.http.get(
+        environment.webApiBaseUrl + environment.apiEndPoint.GetLink,
+        { headers: headers }
+      );
+    }
   }
 
-  if (pageName === "Shelf") {
-   return this.http.get(environment.webApiBaseUrl + environment.apiEndPoint.GetShelf, { headers: headers });
+  getMenuItems(): Observable<any> {
+    return this.http.get(
+      environment.baseUrl + environment.apiEndPoint.getMenu,
+      { headers: menuHeaders }
+    );
   }
 
-  if (pageName === "Port") {
-   return this.http.get(environment.webApiBaseUrl + environment.apiEndPoint.GetPorts, { headers: headers });
+  createPageItems(id): Observable<any> {
+    return this.http.get(
+      environment.baseUrl + environment.apiEndPoint.createPage + '/' + id,
+      { headers: headers }
+    );
   }
 
-  if (pageName === "Card") {
-   return this.http.get(environment.webApiBaseUrl + environment.apiEndPoint.GetCard, { headers: headers });
+  createPageValues(actionLink, data): Observable<any> {
+    return this.http.post(actionLink, data, { headers: headers });
   }
 
-  if (pageName === "Link") {
-   return this.http.get(environment.webApiBaseUrl + environment.apiEndPoint.GetLink, { headers: headers });
-  }
- }
+  removeNE(data): Observable<any> {
+    const httpOptions: any = {
+      headers: headers,
+    };
+    const uri = environment.webApiBaseUrl + environment.apiEndPoint.removeNE;
 
- getMenuItems(): Observable<any> {
-  return this.http.get(environment.baseUrl + environment.apiEndPoint.getMenu, { headers: menuHeaders });
- }
+    httpOptions.body = data;
 
- createPageItems(id): Observable<any> {
-  return this.http.get(environment.baseUrl + environment.apiEndPoint.createPage + "/" + id, { headers: headers });
- }
-
- createPageValues(actionLink, data): Observable<any> {
-  return this.http.post(actionLink, data, { headers: headers });
- }
-
- getDeviceListByID(pageName, id): Observable<any> {
-  if (pageName === "Device") {
-   return this.http.get(environment.baseUrl + environment.apiEndPoint.getDeviceDtl + "/" + id, { headers: headers });
+    return this.http.request<string>('delete', uri, httpOptions);
   }
 
-  if (pageName === "Shelf") {
-   return this.http.get(environment.webApiBaseUrl + environment.apiEndPoint.GetShelf, { headers: headers });
+  getDeviceListByID(pageName, id): Observable<any> {
+    if (pageName === 'Device') {
+      return this.http.get(
+        environment.baseUrl + environment.apiEndPoint.getDeviceDtl + '/' + id,
+        { headers: headers }
+      );
+    }
+
+    if (pageName === 'Shelf') {
+      return this.http.get(
+        environment.webApiBaseUrl + environment.apiEndPoint.GetShelf,
+        { headers: headers }
+      );
+    }
+
+    if (pageName === 'Port') {
+      return this.http.get(
+        environment.webApiBaseUrl + environment.apiEndPoint.GetPorts,
+        { headers: headers }
+      );
+    }
+
+    if (pageName === 'Card') {
+      return this.http.get(
+        environment.webApiBaseUrl + environment.apiEndPoint.GetCard,
+        { headers: headers }
+      );
+    }
+
+    if (pageName === 'Link') {
+      return this.http.get(
+        environment.webApiBaseUrl + environment.apiEndPoint.GetLink,
+        { headers: headers }
+      );
+    }
   }
 
-  if (pageName === "Port") {
-   return this.http.get(environment.webApiBaseUrl + environment.apiEndPoint.GetPorts, { headers: headers });
-  }
+  getDeviceDataByID(pageName, id): Observable<any> {
+    if (pageName === 'Device') {
+      return this.http.get(
+        environment.baseUrl + environment.apiEndPoint.getDeviceData + '/' + id,
+        { headers: headers }
+      );
+    }
 
-  if (pageName === "Card") {
-   return this.http.get(environment.webApiBaseUrl + environment.apiEndPoint.GetCard, { headers: headers });
-  }
+    if (pageName === 'Shelf') {
+      return this.http.get(
+        environment.webApiBaseUrl + environment.apiEndPoint.GetShelf,
+        { headers: headers }
+      );
+    }
 
-  if (pageName === "Link") {
-   return this.http.get(environment.webApiBaseUrl + environment.apiEndPoint.GetLink, { headers: headers });
-  }
- }
+    if (pageName === 'Port') {
+      return this.http.get(
+        environment.webApiBaseUrl + environment.apiEndPoint.GetPorts,
+        { headers: headers }
+      );
+    }
 
- getDeviceDataByID(pageName, id): Observable<any> {
-  if (pageName === "Device") {
-   return this.http.get(environment.baseUrl + environment.apiEndPoint.getDeviceData + "/" + id, { headers: headers });
-  }
+    if (pageName === 'Card') {
+      return this.http.get(
+        environment.webApiBaseUrl + environment.apiEndPoint.GetCard,
+        { headers: headers }
+      );
+    }
 
-  if (pageName === "Shelf") {
-   return this.http.get(environment.webApiBaseUrl + environment.apiEndPoint.GetShelf, { headers: headers });
+    if (pageName === 'Link') {
+      return this.http.get(
+        environment.webApiBaseUrl + environment.apiEndPoint.GetLink,
+        { headers: headers }
+      );
+    }
   }
-
-  if (pageName === "Port") {
-   return this.http.get(environment.webApiBaseUrl + environment.apiEndPoint.GetPorts, { headers: headers });
-  }
-
-  if (pageName === "Card") {
-   return this.http.get(environment.webApiBaseUrl + environment.apiEndPoint.GetCard, { headers: headers });
-  }
-
-  if (pageName === "Link") {
-   return this.http.get(environment.webApiBaseUrl + environment.apiEndPoint.GetLink, { headers: headers });
-  }
- }
 }
